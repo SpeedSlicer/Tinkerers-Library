c9c5e373f8ed9310baf605b34b58a40d18dba1cf
SpeedSlicer <smathew01@student.dasd.org>
2026-05-12 16:19:26 -0400

    j

--- Assets/Scripts/Items/Construct/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Tinker/Item")]
-public class ItemData : ScriptableObject {
-    public string registryName;
-    public Sprite icon;         
-    public GameObject prefab;  
-    public readonly int maxStackSize = 16;
-}
